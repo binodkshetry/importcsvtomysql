36bd24267868a7206a2a74064e37f073997e4c7f
Binod Kshetry <binodkshetry@users.noreply.github.com>
2023-10-29 18:05:33 +1100

    Update importcsvfile.py

--- importcscfile.py
+++ importcscfile.py
@@ -1,7 +1,3 @@
-How to import a CSV file into a MySQL database using python script?
-Using iris data, we will learn how to import a CSV file into a MySQL database using python script.
+Importing CSV into MySQL Using Workbench.
 _______________________________________________________________________________________________________
 
-import pandas as pd
-irisData = pd.read_csv('https://github.com/Muhd-Shahid/Write-Raw-File-into-Database-Server/raw/main/iris.csv', index_col=False)
-irisData.head()
